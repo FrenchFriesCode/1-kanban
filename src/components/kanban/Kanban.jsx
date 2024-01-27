7e0a3d0f0850d597b28a1c5896c79b5f992dbb16
Ismael Fries <152796966+FrenchFriesCode@users.noreply.github.com>
2024-01-27 12:30:13 -0300

    se agregaron funciones droppables y estilos

--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -2,47 +2,61 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import mockData from "../../mockData";
 import { Card } from "../card/Card";
 import { useState } from "react";
-export function Kanban(){
-    const [data,setData]=useState(mockData)
-    const onDragEnd=(result)=>{
-        if(!result.destination) return
-        const {source,destination} =result
-        if(source.droppableId !== destination.droppableId)
-        {
-            const sourceColIndex=data.findIndex(e=>e.id === source.droppableId)
-            const destinationColIndex = data.findIndex(e=>e.id===destination.droppableId)
-            const sourceCol = data[sourceColIndex]
-            const destinationCol=data[destinationColIndex]
+export function Kanban() {
+  const [data, setData] = useState(mockData);
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const destinationColIndex = data.findIndex(
+        (e) => e.id === destination.droppableId
+      );
+      const sourceCol = data[sourceColIndex];
+      const destinationCol = data[destinationColIndex];
 
-            const sourceTask=[...sourceCol.tasks]
-            const destinationTask=[...destinationCol.tasks]
+      const sourceTask = [...sourceCol.tasks];
+      const destinationTask = [...destinationCol.tasks];
 
-            const [removed] = sourceTask.splice(source.index,1)
-            destinationTask.splice(destination.index,0,removed)
+      const [removed] = sourceTask.splice(source.index, 1);
+      destinationTask.splice(destination.index, 0, removed);
 
-            data[sourceColIndex].tasks=sourceTask
-            data[destinationColIndex].tasks=destinationTask
+      data[sourceColIndex].tasks = sourceTask;
+      data[destinationColIndex].tasks = destinationTask;
 
-            setData(data)
-        }
+      setData(data);
     }
-    return(<DragDropContext onDragEnd={onDragEnd}>
-        <div>
-           {data.map((section)=>(
-            <Droppable key={section.id} droppableId={section.id}>{(provided)=>(
-                <div
-                    {...provided.droppableProps}
-                    className="kanban_section"
-                    ref={provided.innerRef}>
-                        <div className="kanban_section_title">{section.title}</div>
+  };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        {data.map((section) => (
+          <Droppable key={section.id} droppableId={section.id}>
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                className="kanban_section"
+                ref={provided.innerRef}
+              >
+                <div className="kanban_section_title">{section.title}</div>
+                <div className="kanban_section_content">{section.tasks.map((task,index)=>(
+                    <Draggable key={task.id} draggableId={task.id} index={index}>
+                        {(provided,snapshot)=>(
+                            <div ref={provided.innerRef}{...provided.draggableProps}{...provided.dragHandleProps} style={
+                                {...provided.draggableProps.style,opacity:snapshot.isDragging?"0.5":"1"}
+                            }>
+                                <Card>{task.title}</Card>
 
+                            </div>
+                        )}
+                    </Draggable>
+                ))}
                 </div>
-            )
-            
-            }
-            
-            </Droppable>
-           ))} 
-        </div>
-    </DragDropContext>)
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  );
 }
